976357ad50b672a0a2cc09e056575c0f6309bc18
ciberjbr <ciberjbr@gmail.com>
2023-10-23 01:36:18 +0200

    add js

--- reque1.js
+++ reque1.js
@@ -1,23 +1,24 @@
-function cargar(){
-    /* CREACIÓN DE NODO */
+        function cargar(){
+        /* CREACIÓN DE NODO */
+    
+        // Crear nodo de tipo Element
+        var encabezado = document.createElement("h1");//<h1></h1>
+        encabezado.id = "titulo" //<p id="encabezado"></p>
+        
+        // Crear nodo de tipo Text
+        var contenido_encabezado = document.createTextNode("Hola Mundo!");//Hola Mundo!
+        
+        // Añadir el nodo Text como hijo del nodo Element
+        encabezado.appendChild(contenido_encabezado);//<h1 id="encabezado">Hola Mundo!</h1>
+        
+        // Añadir el nodo Element como hijo de la pagina
+        document.body.div_inicial.appendChild(encabezado);
+    
+        /* BORRADO DE NODO */
+        //enlace.parentNode.removeChild(enlace);
+        
+    };
 
-    // Crear nodo de tipo Element
-    var encabezado = document.createElement("h1");//<h1></h1>
-    encabezado.id = "encabezado" //<p id="encabezado"></p>
-    
-    // Crear nodo de tipo Text
-    var contenido_encabezado = document.createTextNode("Hola Mundo!");//Hola Mundo!
-    
-    // Añadir el nodo Text como hijo del nodo Element
-    encabezado.appendChild(contenido_encabezado);//<h1 id="encabezado">Hola Mundo!</h1>
-    
-    // Añadir el nodo Element como hijo de la pagina
-    document.body.div_inicial.appendChild(encabezado);
-
-    /* BORRADO DE NODO */
-    //enlace.parentNode.removeChild(enlace);
-    
-};
 
 
 
